ba6a15a7312a1a3387d8d0034ebff68f96345174
sarpisik <contact@sarpisik.com>
2019-06-02 09:47:23 +0200

    Add CodeMaker class

--- Intermediate_Ruby_Projects/mastermind.rb
+++ Intermediate_Ruby_Projects/mastermind.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+def generate_code
+  code = ''
+  4.times { code += rand(1..6).to_s }
+  code
+end
+
 class String
   def red
     "\e[31m#{self}\e[0m"
@@ -16,22 +22,52 @@ class CodeMaker
     puts @code
   end
 
-  def generate_code
-    code = ''
-    4.times { code += rand(7).to_s }
-    code
-  end
-
-  def check_code(guesses)
-    guesses.chars.map.with_index do |char, i|
+  def check_code(guess)
+    guess.chars.map.with_index do |char, i|
       next char.red unless @code.include? char
-      next char.green if guesses[i] == @code[i]
-      next char.red if guesses.scan(char).count > @code.scan(char).count
+      next char.green if guess[i] == @code[i]
+      next char.red if guess.scan(char).count > @code.scan(char).count
 
       char
     end.join('')
   end
 end
 
+class CodeBreaker
+  def initialize(player = false)
+    @machine = player
+  end
+
+  def get_guess
+    return get_machine_guess unless @machine
+
+    get_player_guess
+  end
+
+  def get_machine_guess
+    generate_code
+  end
+
+  def get_player_guess
+    loop do
+      ask_guess
+      break if guess_valid?
+    end
+    @guess
+  end
+
+  def ask_guess
+    puts 'Enter 4 numbers within the range of 1 to 6 to break the secret code!'
+    @guess = gets.chomp
+  end
+
+  def guess_valid?
+    @guess.chars.all? { |char| char.to_i > 0 && char.to_i < 7 }
+  end
+end
+
 c = CodeMaker.new('4340')
 puts c.check_code('1334')
+
+b = CodeBreaker.new(true)
+puts b.get_guess
